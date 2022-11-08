6cd9f1ab4cece50d4674f5e352a2769617f138e4
Matthew Rodgers <mr4683@nyu.edu>
2022-11-08 14:56:27 -0500

    Register page fix

--- pages/register.tsx
+++ pages/register.tsx
@@ -3,25 +3,41 @@ import { useRouter } from "next/router";
 import Head from "next/head";
 import {
   FormErrorMessage,
-  InputLeftAddon,
   FormControl,
-  InputGroup,
-  FormLabel,
   Container,
+  FormLabel,
   useToast,
   Heading,
   Button,
-  chakra,
   HStack,
+  chakra,
   Input,
-  Box,
+  Image,
   Flex,
+  Icon,
+  Text,
+  Box,
+  Checkbox,
+  Divider,
+  Stack,
+  useBreakpointValue,
+  useColorModeValue,
+  InputGroup,
+  InputRightElement,
+  IconButton,
+  useDisclosure,
+  useMergeRefs,
+  ButtonGroup, 
+  VisuallyHidden,
+  InputLeftAddon,
 } from "@chakra-ui/react";
 import Link from "../components/Link";
 import { useForm } from "react-hook-form";
 import { useAuth } from "../contexts/AuthContext";
 import { getAuth } from "firebase/auth";
 import GoogleButton from 'react-google-button'
+import { Logo } from '../components/login/Logo'
+import { GoogleIcon } from '../components/login/ProviderIcons'
 
 
 const Register: NextPage = () => {
@@ -37,13 +53,16 @@ const Register: NextPage = () => {
   const { facebookSignIn: facebook } = useAuth();
 
   const onSubmit = (data: any) => userRegister(data);
+  const providers = [
+    { name: 'Google', icon: <GoogleIcon boxSize="5" /> },
+  ]
 
   return (
     <>
       <Head>
         <title>Awake | Register</title>
       </Head>
-      <Box
+      {/* <Box
         bg="sage.500"
         bgGradient="linear-gradient(41deg,rgb(100, 43, 115) 0%,rgb(164,191,217) 100%)"
         // bgImage="url(https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)"
@@ -74,7 +93,7 @@ const Register: NextPage = () => {
               <FormControl mb='16px'>
                 <FormLabel htmlFor="username">Username:</FormLabel>
                 <InputGroup>
-                  {/* eslint-disable-next-line react/no-children-prop*/}
+                  {/* eslint-disable-next-line react/no-children-prop}
                   <InputLeftAddon children='@' />
                   <Input
                     id='username'
@@ -143,13 +162,108 @@ const Register: NextPage = () => {
                   <GoogleButton onClick={() => { googleRegister() }}></GoogleButton>
                 </Container>
                 <Container mt="3%" ml="-1.75%">
-                  {/* <Button onClick={() => { facebook() }}>Log in with facebook</Button> */}
+                  {/* <Button onClick={() => { facebook() }}>Log in with facebook</Button>}
                 </Container>
               </Flex>
             </chakra.form>
           </Box>
         </Container>
+      </Box> */}
+      <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }} mt="30px">
+    <Stack spacing="8">
+      <Stack spacing="6">
+        <Logo />
+        <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+          <Heading size={useBreakpointValue({ base: 'xs', md: 'sm' })}>
+            Create An Account
+          </Heading>
+          <HStack spacing="1" justify="center">
+            <Text color="muted">Already have an account?</Text>
+            <Button variant="link" colorScheme="blue" onClick={() => {router.push(`/register`);}}>
+              Sign In
+            </Button>
+          </HStack>
+        </Stack>
+      </Stack>
+      <Box
+        py={{ base: '0', sm: '8' }}
+        px={{ base: '4', sm: '10' }}
+        bg={useBreakpointValue({ base: 'transparent', sm: 'bg-surface' })}
+        boxShadow={{ base: 'none', sm: useColorModeValue('md', 'md-dark') }}
+        borderRadius={{ base: 'none', sm: 'xl' }}
+      >
+        <chakra.form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing="6">
+          <Stack spacing="5">
+          <FormControl>
+              <FormLabel htmlFor="email">Username</FormLabel>
+                  <Input
+                    id='username'
+                    {...register("username", {
+                      required: "Username is required",
+                    })}
+                  />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input
+                  id='email'
+                  type='email'
+                  {...register('email', {
+                    required: 'Please enter your email',
+                  })}
+                />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <InputGroup>
+                <InputRightElement>
+                  {/* <IconButton
+                    variant="link"
+                    aria-label={isOpen ? 'Mask password' : 'Reveal password'}
+                    icon={isOpen ? <HiEyeOff /> : <HiEye />}
+                    onClick={onClickReveal}
+                  /> */}
+                </InputRightElement>
+                <Input
+                  id='password'
+                  type='password'
+                  {...register('password', {
+                    required: 'Please enter your password',
+                  })}
+                />
+              </InputGroup>
+          </FormControl>
+          </Stack>
+          <HStack justify="space-between">
+            <Checkbox defaultChecked>Remember me</Checkbox>
+            {/* <Button variant="link" colorScheme="blue" size="sm">
+              Forgot password?
+            </Button> */}
+          </HStack>
+          <Stack spacing="6">
+            <Button variant="primary" type="submit">Create An Account</Button>
+            <HStack>
+              <Divider />
+              <Text fontSize="sm" whiteSpace="nowrap" color="muted">
+                or continue with
+              </Text>
+              <Divider />
+            </HStack>
+            <ButtonGroup variant="outline" spacing="4" width="full">
+              {providers.map(({ name, icon }) => (
+                <Button key={name} width="full" onClick={() => { googleRegister() }}>
+                  <VisuallyHidden>Sign in with {name}</VisuallyHidden>
+                  {icon}
+                </Button>
+              ))}
+            </ButtonGroup>
+          </Stack>
+        </Stack>
+        </chakra.form>
       </Box>
+    </Stack>
+  </Container>
     </>
   );
 };
