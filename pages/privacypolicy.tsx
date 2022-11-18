2baa31344fc4ca034dd50993503411a54ce7f6b2
Matthew Rodgers <mr4683@nyu.edu>
2022-11-18 10:57:52 -0500

    privacy policy change

--- pages/privacypolicy.tsx
+++ pages/privacypolicy.tsx
@@ -17,13 +17,13 @@ class privacyPolicy extends React.Component<Props, State> {
       <>
       <Box bgGradient="linear-gradient(41deg,rgb(100, 43, 115) 0%,rgb(164,191,217) 100%)" height="98px" width="100%"></Box>
       <Container mt="5%">
-        <h1>Privacy Policy for ClimateDAO Incorporated</h1>
+        <h1>Privacy Policy for Awake</h1>
 
-        <p>At climatedao.xyz, accessible from https://climatedao.xyz/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by climatedao.xyz and how we use it.</p>
+        <p>At awakeinvest.com, accessible from https://awakeinvest.com/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by awakeinvest.com and how we use it.</p>
 
         <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
 
-        <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in climatedao.xyz. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the <a href="https://www.privacypolicygenerator.info/">Free Privacy Policy Generator</a>.</p>
+        <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in awakeinvest.com. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the <a href="https://www.privacypolicygenerator.info/">Free Privacy Policy Generator</a>.</p>
 
         <h2>Consent</h2>
 
@@ -51,25 +51,25 @@ class privacyPolicy extends React.Component<Props, State> {
 
         <h2>Log Files</h2>
 
-        <p>climatedao.xyz follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services&apos; analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users&apos; movement on the website, and gathering demographic information.</p>
+        <p>awakeinvest.com follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services&apos; analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users&apos; movement on the website, and gathering demographic information.</p>
 
         <h2>Cookies and Web Beacons</h2>
 
-        <p>Like any other website, climatedao.xyz uses &apos;cookies&apos;. These cookies are used to store information including visitors&apos; preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users&apos; experience by customizing our web page content based on visitors&apos; browser type and/or other information.</p>
+        <p>Like any other website, awakeinvest.com uses &apos;cookies&apos;. These cookies are used to store information including visitors&apos; preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users&apos; experience by customizing our web page content based on visitors&apos; browser type and/or other information.</p>
 
 
 
         <h2>Advertising Partners Privacy Policies</h2>
 
-        <p>You may consult this list to find the Privacy Policy for each of the advertising partners of climatedao.xyz.</p>
+        <p>You may consult this list to find the Privacy Policy for each of the advertising partners of awakeinvest.com.</p>
 
-        <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on climatedao.xyz, which are sent directly to users&apos; browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+        <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on awakeinvest.com, which are sent directly to users&apos; browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
 
-        <p>Note that climatedao.xyz has no access to or control over these cookies that are used by third-party advertisers.</p>
+        <p>Note that awakeinvest.com has no access to or control over these cookies that are used by third-party advertisers.</p>
 
         <h2>Third Party Privacy Policies</h2>
 
-        <p>climatedao.xyz&apos;s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
+        <p>awakeinvest.com&apos;s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
 
         <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers&apos; respective websites.</p>
 
@@ -96,7 +96,7 @@ class privacyPolicy extends React.Component<Props, State> {
 
         <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
-        <p>climatedao.xyz does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+        <p>awakeinvest.com does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
         </Container>
       </>
     )
